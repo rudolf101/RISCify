5cad67ad21d770d62e3b0303acefb552b3cc68dc
Vladislav Kasimov <70969943+rudolf101@users.noreply.github.com>
2024-10-20 19:29:44 +0300

    Disassembler init (#26)

---
+++ disassembler/Disassembler.ts
@@ -0,0 +1,17 @@
+import {Instruction} from "./model/Instruction";
+import {BytesSplitter} from "./utils/BytesSplitter";
+
+class Disassembler {
+
+    private static findInstruction(byteBuffer: Uint8Array[]): Instruction {
+        return null;
+    }
+
+    static disassemble(byteBuffer: Uint8Array[]): Instruction[] {
+        let instructionsBytes: Uint8Array[][] = BytesSplitter.splitBytes(byteBuffer);
+
+        return instructionsBytes.map(this.findInstruction)
+    }
+}
+
+export {Disassembler};
