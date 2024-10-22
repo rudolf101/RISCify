4a970911843994b856fe962746ed7b381cb7465d
Олейников Андрей Павлович <a.p.oleynikov@tbank.ru>
2024-10-22 18:11:06 +0300

    rollback

--- disassembler/Disassembler.ts
+++ disassembler/Disassembler.ts
@@ -1,13 +1,13 @@
-import {Instruction} from "./model/Instruction";
-import {BytesSplitter} from "./utils/BytesSplitter";
+import { Instruction } from "./model/Instruction";
+import { BytesSplitter } from "./utils/BytesSplitter";
 
 class Disassembler {
 
-    private static findInstruction(byteBuffer: Uint8Array[]): Instruction | null {
+    private static findInstruction(byteBuffer: Uint8Array[]): Instruction {
         return null;
     }
 
-    static disassemble(byteBuffer: Uint8Array[]): Instruction[] | null {
+    static disassemble(byteBuffer: Uint8Array[]): Instruction[] {
         let instructionsBytes: Uint8Array[][] = BytesSplitter.splitBytes(byteBuffer);
 
         // return instructionsBytes.map(this.findInstruction)
@@ -15,4 +15,4 @@ class Disassembler {
     }
 }
 
-export {Disassembler};
+export { Disassembler };
