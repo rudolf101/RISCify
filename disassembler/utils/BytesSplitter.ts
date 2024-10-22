302b09484e8e3f47042fd978c10433fa1e0c6f47
CaesiumFox <48699925+CaesiumFox@users.noreply.github.com>
2024-10-22 21:08:58 +0300

    Реализация findInstruction (#28)

--- disassembler/utils/BytesSplitter.ts
+++ disassembler/utils/BytesSplitter.ts
@@ -1,12 +1,21 @@
 class BytesSplitter {
-
-    public static splitBytes(byteArrays: Uint8Array[]): Uint8Array[][] {
-        const instructions: Uint8Array[][] = [];
+    /**
+     * @param byteArrays array of bytes, each represented by Uint8Array(8),
+     * where bits are ordered from least to greatest.
+     * e.g. byte 123 = 01111011_2 is represented as [1,1,0,1,1,1,1,0]
+     * @returns array of bit sequences for instruction decoding
+     * with bits ordered from least to greatest.
+     */
+    public static splitBytes(byteArrays: Uint8Array[]): Uint8Array[] {
+        const emptyByte = new Uint8Array(8);
+        const instructions: Uint8Array[] = [];
         const instructionSize = 4; // Only RV32i is supported for now
 
         for (let i = 0; i < byteArrays.length; i += instructionSize) {
-            const instruction = byteArrays.slice(i, i + instructionSize);
-            instructions.push(instruction.reverse()); // Little-Endian
+            var instruction = new Uint8Array(32);
+            for (var j = 0; j < instructionSize; j++)
+                instruction.set(byteArrays[i + j] ?? emptyByte, 8 * j);
+            instructions.push(instruction);
         }
 
         return instructions;
