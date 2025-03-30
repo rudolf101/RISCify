a8fc0fc427a73934341263017a2018230fbd9c36
Dark Hole <dark.hole1@yandex.ru>
2025-03-30 15:04:25 +0300

    Add theoretical disasm

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,259 +1,26 @@
-import React, { useState } from 'react';
-import {
-  Container,
-  TextField,
-  Button,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableRow,
-  Paper,
-  Typography,
-  Box,
-  Tooltip,
-} from '@mui/material';
-
-import useDisassembler from './Render';
-import { Provider } from './generator/go';
-import { Apply } from './generator/models';
 import "./App.css";
+import { descriptions } from "./kernel/Descriptions";
+import { disassemble, SimilarInstructions } from "./kernel/Disassembler";
 
-const provider = new Provider()
+import { InputOrder, inputParser } from "./kernel/InputParser";
+import { BitDepth } from "./kernel/InstructionDescription";
+import { splitter } from "./kernel/Splitter";
 
-type InstructionCardProps = {
-  apply: Apply;
-};
-
-
-const InstructionCard: React.FC<InstructionCardProps> = ({ apply }) => {
-  const [expanded, setExpanded] = useState(false);
-  const [copied, setCopied] = useState(false);
-
-  const copyToClipboard = (text: string, event: React.MouseEvent) => {
-    event.stopPropagation();
-    navigator.clipboard.writeText(text)
-      .then(() => {
-        setCopied(true);
-        setTimeout(() => setCopied(false), 1000);
-      })
-      .catch((err) => {
-        alert('Failed to copy: ' + err);
-      });
-  };
-
-  const toggleExpanded = () => {
-    setExpanded((prev) => !prev);
-  };
-
-  return (
-    <Paper
-      elevation={0}
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        padding: '1rem',
-        margin: '0.5rem',
-        textAlign: 'left',
-        transition: 'background-color 0.3s, transform 0.3s',
-        border: '1px solid rgba(0, 0, 0, 0.3)',
-        borderRadius: '15px',
-        '&:hover': {
-          backgroundColor: 'rgba(0, 0, 0, 0.05)',
-          transform: 'scale(1.02)',
-        },
-        cursor: 'pointer',
-      }}
-      onClick={toggleExpanded}
-    >
-      <Box
-        sx={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          flexGrow: 1,
-        }}
-      >
-        <Typography
-          sx={{
-            fontWeight: 'bold',
-          }}
-        >
-          {apply.Instruction.mnemonic} {apply.Args.map((arg) => arg.value).join(' ')}
-        </Typography>
-      </Box>
-
-      {expanded && (
-        <>
-          <Typography variant="body1" sx={{ marginBottom: '0.5rem', fontWeight: 'bold' }}>
-            <strong>Fields:</strong>
-          </Typography>
-          <Box
-            sx={{
-              display: 'grid',
-              gridTemplateColumns: 'repeat(auto-fill, minmax(150px, 1fr))',
-              gap: '1rem',
-            }}
-          >
-            {apply.Instruction.fields.map((fieldName, fieldIndex) => {
-              try {
-                const field = provider.getField(fieldName);
-                return (
-                  <Tooltip
-                    key={fieldIndex}
-                    title={copied ? 'Copied!' : 'Copy'}
-                    sx={{ cursor: 'pointer' }}
-                    arrow
-                  >
-                    <Box
-                      sx={{
-                        padding: '0.5rem',
-                        border: '1px solid #e0e0e0',
-                        borderRadius: '8px',
-                        backgroundColor: '#f9f9f9',
-                        display: 'flex',
-                        flexDirection: 'column',
-                        justifyContent: 'space-between',
-                        '&:hover': {
-                          backgroundColor: '#e0f7fa',
-                        },
-                        '&:active': {
-                          backgroundColor: '#b2ebf2',
-                        },
-                      }}
-                      onClick={(event) => copyToClipboard(field.value, event)}
-                    >
-                      <Typography variant="body2" sx={{ fontWeight: 'bold' }}>
-                        {field.name}:
-                      </Typography>
-                      <Typography variant="body2" sx={{ color: '#555', marginBottom: '0.5rem' }}>
-                        {field.value.split('').reverse().join('')}
-                      </Typography>
-                    </Box>
-                  </Tooltip>
-                );
-              } catch (e) {
-                return (
-                  <Box
-                    key={fieldIndex}
-                    sx={{
-                      padding: '0.5rem',
-                      border: '1px solid red',
-                      borderRadius: '8px',
-                      backgroundColor: '#ffe6e6',
-                      display: 'flex',
-                      flexDirection: 'column',
-                      justifyContent: 'space-between',
-                    }}
-                  >
-                    <Typography variant="body2" sx={{ fontWeight: 'bold', color: 'red' }}>
-                      Field not found: {fieldName}
-                    </Typography>
-                  </Box>
-                );
-              }
-            })}
-          </Box>
-
-          <Typography variant="body1" sx={{ marginTop: '1rem', marginBottom: '0.5rem', fontWeight: 'bold' }}>
-            <strong>Arguments:</strong>
-          </Typography>
-          <Box
-            sx={{
-              display: 'grid',
-              gridTemplateColumns: 'repeat(auto-fill, minmax(150px, 1fr))',
-              gap: '1rem',
-            }}
-          >
-            {apply.Instruction.args.map((argName, argIndex) => {
-              try {
-                const param = provider.getParam(argName);
-                return (
-                  <Tooltip
-                    key={argIndex}
-                    title={copied ? 'Copied!' : 'Copy'}
-                    sx={{ cursor: 'pointer' }}
-                    arrow
-                  >
-                    <Box
-                      sx={{
-                        padding: '0.5rem',
-                        border: '1px solid #e0e0e0',
-                        borderRadius: '8px',
-                        backgroundColor: '#f9f9f9',
-                        display: 'flex',
-                        flexDirection: 'column',
-                        justifyContent: 'space-between',
-                        '&:hover': {
-                          backgroundColor: '#e0f7fa',
-                        },
-                        '&:active': {
-                          backgroundColor: '#b2ebf2',
-                        },
-                      }}
-                      onClick={(event) => copyToClipboard(apply.Args[argIndex].value, event)}  // Копируем при клике на текст
-                    >
-                      <Typography variant="body2" sx={{ fontWeight: 'bold' }}>
-                        {param.name}:
-                      </Typography>
-                      <Typography variant="body2" sx={{ color: '#555', marginBottom: '0.5rem' }}>
-                        {apply.Args[argIndex].value}
-                      </Typography>
-                    </Box>
-                  </Tooltip>
-                );
-              } catch (e) {
-                return (
-                  <Box
-                    key={argIndex}
-                    sx={{
-                      padding: '0.5rem',
-                      border: '1px solid red',
-                      borderRadius: '8px',
-                      backgroundColor: '#ffe6e6',
-                      display: 'flex',
-                      flexDirection: 'column',
-                      justifyContent: 'space-between',
-                    }}
-                  >
-                    <Typography variant="body2" sx={{ fontWeight: 'bold', color: 'red' }}>
-                      Argument not found: {argName}
-                    </Typography>
-                  </Box>
-                );
-              }
-            })}
-          </Box>
-        </>
-      )}
-    </Paper>
-  );
-};
-
-
-const App: React.FC = () => {
-  const { hexCode, setHexCode, instructions, disassemble: handleDisassemble } = useDisassembler();
-  const [error, setError] = useState<string | null>(null);
-
-  const handleHexChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value;
-
-    /* Error not used
-    const isValidHex = /^[0-9a-fA-F\s]*$/.test(value);
-  
-    const isEvenLength = value.replace(/\s+/g, '').length % 2 === 0;
-  
-    if (!isValidHex) {
-      setError('Please enter a valid hex code (0-9, a-f, A-F, and spaces)');
-    } else if (!isEvenLength) {
-      setError('Hex code should contain an even number of characters (pairs of digits)');
-    } else {
-      setError(null);
-    }
-    */
-
-    setHexCode(value);
-  };
+const App = () => {
+  const sourceCode = "0010031302b3566300331e1301c50e33ff8e3e83000e3f0301df58630010029301de3023ffee3c2300130313fd9ff06ffc0296e300008067"
+  const input = inputParser(sourceCode, {
+    order: InputOrder.BYTE_ORDER_BE,
+    parcelSkip: 0
+  })
+  let disassemblerResult: SimilarInstructions[] | null
+  if (input.valid === 'valid') {
+    let splitterResult = splitter(input)
+    disassemblerResult = disassemble(splitterResult, descriptions, {
+      bitDepth: BitDepth.BIT_32
+    })
+  } else {
+    disassemblerResult = null
+  }
 
   return (
     <div className='app'>
@@ -266,10 +33,10 @@ const App: React.FC = () => {
         </div>
         <div className="tool">
           <span>SKIP PARCELS</span>
-            <input value={0} />
+          <input value={0} />
         </div>
         <div className="tool">
-        <span>BYTE ORDER</span>
+          <span>BYTE ORDER</span>
           <div className='switch'>
             <span className='selected'>BE</span>
             <span>LE</span>
