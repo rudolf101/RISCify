d5dbdc030642983903713c5a7a297482bc0517f5
Dark Hole <dark.hole1@yandex.ru>
2025-03-30 15:40:24 +0300

    Add decoding

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,22 +1,38 @@
 import "./App.css";
 import { descriptions } from "./kernel/Descriptions";
-import { disassemble, SimilarInstructions } from "./kernel/Disassembler";
+import { disassemble, FilterSettings, SimilarInstructions } from "./kernel/Disassembler";
 
-import { InputOrder, inputParser } from "./kernel/InputParser";
-import { BitDepth } from "./kernel/InstructionDescription";
+import { InputOrder, inputParser, ValidInput } from "./kernel/InputParser";
+import { BitDepth, InstructionDescription } from "./kernel/InstructionDescription";
 import { splitter } from "./kernel/Splitter";
+
+const Message = (props: {
+  header: string, text: string, error?: boolean
+}) => {
+  return <>
+    <div className={'header ' + (props.error ? 'error' : '')}>{props.header}</div>
+    <div className={'message ' + (props.error ? 'error' : '')}>{props.text}</div>
+  </>
+}
+
+const disassembleChain =
+  (input: ValidInput, descriptions: InstructionDescription[], settings: FilterSettings) =>
+    disassemble(splitter(input), descriptions, settings)
 
 const App = () => {
   const sourceCode = "0010031302b3566300331e1301c50e33ff8e3e83000e3f0301df58630010029301de3023ffee3c2300130313fd9ff06ffc0296e300008067"
+  const byteOrder = InputOrder.BYTE_ORDER_BE;
+  const parcelSkip = 0;
+  const bitDepth = BitDepth.BIT_32;
   const input = inputParser(sourceCode, {
-    order: InputOrder.BYTE_ORDER_BE,
-    parcelSkip: 0
+    order: byteOrder,
+    parcelSkip: parcelSkip
   })
   let disassemblerResult: SimilarInstructions[] | null
   if (input.valid === 'valid') {
     let splitterResult = splitter(input)
     disassemblerResult = disassemble(splitterResult, descriptions, {
-      bitDepth: BitDepth.BIT_32
+      bitDepth: bitDepth
     })
   } else {
     disassemblerResult = null
@@ -51,15 +67,15 @@ const App = () => {
           </div>
         </div>
       </div>
-      {true ? <>
-        <div className='header'>PASTE CODE</div>
-        <div className='message'>CTRL + V</div>
-      </> :
-        <div className='code'>
-          <div className='arrows'>
-            {/* TODO: Add arrows for jumps */}
-          </div>
-          <pre className='offsets'>{`0x0000
+      {sourceCode.length === 0 ?
+        <Message header='PASTE CODE' text='CTRL + V' /> :
+        input.valid === 'invalid' ?
+          <Message header='FAILED TO DECODE' text={input.message} error /> :
+          <div className='code'>
+            <div className='arrows'>
+              {/* TODO: Add arrows for jumps */}
+            </div>
+            <pre className='offsets'>{`0x0000
 0x0004
 0x0008
 0x000c
@@ -74,7 +90,7 @@ const App = () => {
 0x0030
 0x0034
 0x0038`}</pre>
-          <pre className='encoded'>{`00000293
+            <pre className='encoded'>{`00000293
 00100313
 02b35663
 00331e13
@@ -89,39 +105,39 @@ ffee3c23
 fd9ff06f
 fc0296e3
 00008067`}</pre>
-          <div className='decoded'>
-            <div>li</div>
-            <div>t0, 0</div>
-            <div>li</div>
-            <div>t1, 1</div>
-            <div>bge</div>
-            <div>t1, a1, 2f</div>
-            <div>slli</div>
-            <div>t3, t1, 3</div>
-            <div>add</div>
-            <div>t3, a0, t3</div>
-            <div>ld</div>
-            <div>t4, -8(t3)</div>
-            <div>ld</div>
-            <div>t5, 0(t3)</div>
-            <div>ble</div>
-            <div>t4, t5, 3f</div>
-            <div>li</div>
-            <div>t0, 1</div>
-            <div>sd</div>
-            <div>t4, 0(t3)</div>
-            <div>sd</div>
-            <div>t5, -8(t3)</div>
-            <div>addi</div>
-            <div>t1, t1, 1</div>
-            <div>j</div>
-            <div>2b</div>
-            <div>bnez</div>
-            <div>t0, 1b</div>
-            <div>ret</div>
-            <div></div>
+            <div className='decoded'>
+              <div>li</div>
+              <div>t0, 0</div>
+              <div>li</div>
+              <div>t1, 1</div>
+              <div>bge</div>
+              <div>t1, a1, 2f</div>
+              <div>slli</div>
+              <div>t3, t1, 3</div>
+              <div>add</div>
+              <div>t3, a0, t3</div>
+              <div>ld</div>
+              <div>t4, -8(t3)</div>
+              <div>ld</div>
+              <div>t5, 0(t3)</div>
+              <div>ble</div>
+              <div>t4, t5, 3f</div>
+              <div>li</div>
+              <div>t0, 1</div>
+              <div>sd</div>
+              <div>t4, 0(t3)</div>
+              <div>sd</div>
+              <div>t5, -8(t3)</div>
+              <div>addi</div>
+              <div>t1, t1, 1</div>
+              <div>j</div>
+              <div>2b</div>
+              <div>bnez</div>
+              <div>t0, 1b</div>
+              <div>ret</div>
+              <div></div>
+            </div>
           </div>
-        </div>
       }
     </div>
   );
