796b1aaaa248f95ba761cb8dfcbca80589490c3f
CaesiumFox <avdeev20020904@gmail.com>
2025-03-31 17:43:53 +0300

    Add example

--- app/src/kernel/Descriptions.ts
+++ app/src/kernel/Descriptions.ts
@@ -1,5 +1,16 @@
-import { InstructionDescription } from "./InstructionDescription";
+import { ArgumentPattern } from "./ArgumentPattern";
+import { BitDepth, InstructionDescription } from "./InstructionDescription";
+import { Matcher } from "./Matcher";
 
 // TODO
 // YAML описание будет преобразовано в это
-export var descriptions: InstructionDescription[] = [];
+// В примере приведено описание инструкции `add`
+export var descriptions: InstructionDescription[] = [
+    new InstructionDescription("I", BitDepth.BIT_32 | BitDepth.BIT_64, 32, "add")
+    .addField(new Matcher( "opcode", "0:6", "1100110"))
+    .addField(new Matcher( "funct3", "12:14", "000"))
+    .addField(new Matcher("funct7", "25:31", "0000000"))
+    .addArg(new ArgumentPattern("rd", "7:11", "regx"))
+    .addArg(new ArgumentPattern("rs1", "15:19", "regx"))
+    .addArg(new ArgumentPattern("rs2", "20:24", "regx"))
+];
