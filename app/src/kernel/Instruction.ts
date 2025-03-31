63d753cdef46e2632a530afd09be17fc4c08f256
CaesiumFox <avdeev20020904@gmail.com>
2025-03-31 21:04:02 +0300

    Add facade

--- app/src/kernel/Instruction.ts
+++ app/src/kernel/Instruction.ts
@@ -2,13 +2,62 @@ import { BitDepth, InstructionDescription, Jump } from "./InstructionDescription
 import { Bits } from "./Bits";
 import { Argument } from "./Argument";
 
+// Инструкция не является control-flow
+type ActualJumpNone = {
+    label: "none",
+};
+
+// Переход в отрицательную сторону, но за пределы дампа
+type ActualJumpBack = {
+    label: "back",
+};
+
+// Переход в положительную сторону, но за пределы дампа
+type ActualJumpFront = {
+    label: "front",
+};
+
+// Переход в положительную или отрицательную сторону, но в пределах дампа
+type ActualJumpConcrete = {
+    label: "concrete",
+    distance: number // смещение в инструкциях, а не байтах
+};
+
+// Переход невозможно рассчитать в compile-time (напирмер зависит от значения в регистре, как в jalr)
+type ActualJumpOut = {
+    label: "out",
+};
+
+// Переход указыает на место посреди инструкции.
+// Например:
+//   nop   ; 4 байта
+//   j -2  ; перемещаемся на 2 байта назад (на середину nop)
+type ActualJumpBetween = {
+    label: "between",
+    distance: number, // указывает на ближайшую инструкцию вниз
+    offset: number // количество байт от начала инструкции
+};
+
+// Основное отличие от Jump в модуле InstructionDescription:
+// distance даётся в инструкциях, а не байтах
+type ActualJump
+    = ActualJumpNone
+    | ActualJumpBack
+    | ActualJumpFront
+    | ActualJumpConcrete
+    | ActualJumpOut
+    | ActualJumpBetween
+    ;
+
 export class Instruction {
     private _description: InstructionDescription;
     private _args: Argument[];
+    private _actualJump: ActualJump;
 
     constructor(bits: Bits, description: InstructionDescription) {
         this._description = description;
         this._args = description.args.map((pattern) => new Argument(pattern.name, bits, pattern));
+        this._actualJump = { label: "none" };
     }
 
     public get description(): InstructionDescription {
@@ -45,6 +94,14 @@ export class Instruction {
 
     public get jump(): Jump {
         return this._description.jump;
+    }
+
+    public get actualJump(): ActualJump {
+        return this._actualJump;
+    }
+
+    public set actualJump(newActualJump: ActualJump) {
+        this._actualJump = newActualJump;
     }
 
     /**
---
+++ app/src/kernel/Kernel.ts
@@ -0,0 +1,38 @@
+import { adjuster } from "./Adjuster";
+import { descriptions } from "./Descriptions";
+import { disassemble, FilterSettings, SimilarInstructions } from "./Disassembler";
+import { inputParser, InputSettings } from "./InputParser";
+import { splitter } from "./Splitter";
+
+type DisassembleOutputValid = {
+    valid: "valid",
+    result: SimilarInstructions[]
+};
+
+type DisassembleOutputInvalid = {
+    valid: "invalid",
+    message: string
+};
+
+type DisassembleOutput = DisassembleOutputValid | DisassembleOutputInvalid;
+
+export default function performDisassemble(
+    dump: string,
+    inputSettings: InputSettings,
+    filterSettings: FilterSettings
+): DisassembleOutput {
+    var input = inputParser(dump, inputSettings);
+    if (input.valid == "invalid") {
+        return {
+            valid: "invalid",
+            message: input.message
+        };
+    }
+    var split = splitter(input);
+    var disassembled = disassemble(split, descriptions, filterSettings);
+    disassembled = adjuster(disassembled);
+    return {
+        valid: "valid",
+        result: disassembled
+    };
+}
