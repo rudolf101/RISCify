91c82eddef81f258aeb670807d3ab71080636c48
Dark Hole <dark.hole1@yandex.ru>
2025-05-19 16:23:32 +0300

    Dev edit dump (#126)

--- app/src/ui/App.tsx
+++ app/src/ui/App.tsx
@@ -158,7 +158,8 @@ const Arrows = (props: {
     return skippedPackedJumps;
   }, [props.instructions, props.skip]);
 
-  const width = (packedJumps.reduce((a, b) => Math.max(a, b.level), 0) + 1) * fontSize;
+  const width =
+    (packedJumps.reduce((a, b) => Math.max(a, b.level), 0) + 1) * fontSize;
 
   const renderJump = (jump: LevelledJump) => {
     let x1 = width - 1;
@@ -497,6 +498,7 @@ const App = () => {
   const [bytesSkip, setBytesSkip] = useState(0);
   const [bitDepth, setBitDepth] = useState(BitDepth.BIT_32);
   const [display, setDisplay] = useState<Display>("hex");
+  const [edit, setEdit] = useState(false);
   const [jump, setJump] = useState<Jump>("relative");
   const [disassemblerResult, setDisassemblerResult] =
     useState<DisassembleOutput>({
@@ -523,6 +525,7 @@ const App = () => {
     const handler = (e: ClipboardEvent) => {
       if (e.clipboardData) {
         setSourceCode(e.clipboardData.getData("text"));
+        setEdit(false);
       }
     };
 
@@ -535,102 +538,119 @@ const App = () => {
 
   return (
     <div className="app">
-      <div className="tools">
-        <div className="tool">
-          <Switch
-            cases={[
-              { text: "RV32", value: BitDepth.BIT_32 },
-              { text: "RV64", value: BitDepth.BIT_64 },
-            ]}
-            value={bitDepth}
-            onChange={setBitDepth}
-          />
+      <div className="left">
+        <div className="tools">
+          <div className="tool">
+            <Switch
+              cases={[
+                { text: "RV32", value: BitDepth.BIT_32 },
+                { text: "RV64", value: BitDepth.BIT_64 },
+              ]}
+              value={bitDepth}
+              onChange={setBitDepth}
+            />
+          </div>
+          <div className="tool">
+            <span>SKIP BYTES</span>
+            <input
+              type="number"
+              value={bytesSkip}
+              onChange={(e) => {
+                if (e.currentTarget.value.trim().length === 0) {
+                  setBytesSkip(0);
+                  return;
+                }
+                const value = e.currentTarget.valueAsNumber;
+                if (isFinite(value) && value >= 0) {
+                  setBytesSkip(value | 0);
+                }
+              }}
+            />
+          </div>
+          <div className="tool">
+            <span>BYTE ORDER</span>
+            <Switch
+              cases={[
+                {
+                  text: "BE",
+                  value: InputOrder.BYTE_ORDER_BE,
+                },
+                {
+                  text: "LE",
+                  value: InputOrder.BYTE_ORDER_LE,
+                },
+              ]}
+              value={byteOrder}
+              onChange={setByteOrder}
+            />
+          </div>
+          <div className="tool">
+            <span>DISPLAY</span>
+            <Switch
+              cases={[
+                {
+                  text: "0xFF",
+                  value: "hex" as const,
+                },
+                {
+                  text: "0b01",
+                  value: "bin" as const,
+                },
+              ]}
+              value={display}
+              onChange={setDisplay}
+            />
+          </div>
+          <div className="tool">
+            <span>JUMP STYLE</span>
+            <Switch
+              cases={[
+                {
+                  text: "REL",
+                  value: "relative" as const,
+                },
+                {
+                  text: "ABS",
+                  value: "absolute" as const,
+                },
+              ]}
+              value={jump}
+              onChange={setJump}
+            />
+          </div>
+          <div className="tool">
+            <button
+              className={edit ? "selected" : ""}
+              onClick={() => setEdit((edit) => !edit)}
+            >
+              EDIT DUMP
+            </button>
+          </div>
         </div>
-        <div className="tool">
-          <span>SKIP BYTES</span>
-          <input
-            type="number"
-            value={bytesSkip}
-            onChange={(e) => {
-              if (e.currentTarget.value.trim().length === 0) {
-                setBytesSkip(0);
-                return;
-              }
-              const value = e.currentTarget.valueAsNumber;
-              if (isFinite(value) && value >= 0) {
-                setBytesSkip(value | 0);
-              }
-            }}
+        {sourceCode.length === 0 ? (
+          <Message header="PASTE CODE" text="CTRL + V" />
+        ) : disassemblerResult.valid === "invalid" ? (
+          <Message
+            header="FAILED TO DECODE"
+            text={disassemblerResult.message}
+            error
           />
-        </div>
-        <div className="tool">
-          <span>BYTE ORDER</span>
-          <Switch
-            cases={[
-              {
-                text: "BE",
-                value: InputOrder.BYTE_ORDER_BE,
-              },
-              {
-                text: "LE",
-                value: InputOrder.BYTE_ORDER_LE,
-              },
-            ]}
-            value={byteOrder}
-            onChange={setByteOrder}
+        ) : (
+          <Code
+            instructions={disassemblerResult.result}
+            display={display}
+            order={byteOrder}
+            jump={jump}
           />
-        </div>
-        <div className="tool">
-          <span>DISPLAY</span>
-          <Switch
-            cases={[
-              {
-                text: "0xFF",
-                value: "hex" as const,
-              },
-              {
-                text: "0b01",
-                value: "bin" as const,
-              },
-            ]}
-            value={display}
-            onChange={setDisplay}
-          />
-        </div>
-        <div className="tool">
-          <span>JUMP STYLE</span>
-          <Switch
-            cases={[
-              {
-                text: "REL",
-                value: "relative" as const,
-              },
-              {
-                text: "ABS",
-                value: "absolute" as const,
-              },
-            ]}
-            value={jump}
-            onChange={setJump}
-          />
-        </div>
+        )}
       </div>
-      {sourceCode.length === 0 ? (
-        <Message header="PASTE CODE" text="CTRL + V" />
-      ) : disassemblerResult.valid === "invalid" ? (
-        <Message
-          header="FAILED TO DECODE"
-          text={disassemblerResult.message}
-          error
-        />
-      ) : (
-        <Code
-          instructions={disassemblerResult.result}
-          display={display}
-          order={byteOrder}
-          jump={jump}
-        />
-      )}
+      <div className={`right ${edit ? "show" : ""}`}>
+        <textarea
+          value={sourceCode}
+          onPaste={(e) => e.stopPropagation()}
+          onChange={(e) => setSourceCode(e.currentTarget.value)}
+        ></textarea>
+      </div>
     </div>
   );
 };
