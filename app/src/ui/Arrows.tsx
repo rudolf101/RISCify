f026d1a81e50cafe9a8cce15fa39f58b93d8a7d0
Dark Hole <dark.hole1@yandex.ru>
2025-06-15 20:54:19 +0300

    Rewrite arrows

--- app/src/ui/Arrows.tsx
+++ app/src/ui/Arrows.tsx
@@ -8,10 +8,30 @@ type LocalJump = {
   to: number;
   broken: boolean;
 };
+
 export type LevelledJump = LocalJump & {
   level: number;
 };
-export const packJumps = (jumps: LocalJump[]): LevelledJump[] => {
+
+const instructions2jumps = (instructions: SimilarInstructions[]): LocalJump[] => {
+  return instructions.flatMap((e, i) => {
+      if (e.instructions.length < 1) {
+        return [];
+      }
+      const jump = e.instructions[0].actualJump;
+      if (jump.label === "concrete") {
+        return [{ from: i, to: i + jump.distance, broken: false as boolean }];
+      }
+      if (jump.label === "between") {
+        return [{ from: i, to: i + jump.distance, broken: true as boolean }];
+      }
+      return [];
+    });
+}
+
+// A pretty straightforward solution for interval coloring problem
+// Probably O(N^3) where N is number of jumps
+const packJumps = (jumps: LocalJump[]): LevelledJump[] => {
   const sortedJumps = jumps.sort((j1, j2) => {
     const d1 = Math.abs(j1.from - j1.to);
     const d2 = Math.abs(j2.from - j2.to);
@@ -52,29 +72,83 @@ export const packJumps = (jumps: LocalJump[]): LevelledJump[] => {
   return result;
 };
 
+const Marker = (props: { id: string; color: string }) => (
+  <marker
+    id={props.id}
+    orient="auto"
+    markerUnits="userSpaceOnUse"
+    markerWidth="9"
+    markerHeight="16"
+    refX="8"
+    refY="8"
+  >
+    <polyline
+      strokeWidth="2"
+      points="1,1 8,8 1,15"
+      fill="none"
+      stroke={props.color}
+      strokeLinejoin="round"
+      strokeLinecap="round"
+    ></polyline>
+  </marker>
+);
+
+const JumpElement = (props: {
+  width: number;
+  fontSize: number;
+  lineHeight: number;
+  jump: LevelledJump;
+  current: boolean;
+}) => {
+  let x1 = props.width - 1;
+  let x2 = x1 - props.fontSize * props.jump.level;
+  let y1 = props.jump.from * props.lineHeight + props.lineHeight / 2;
+  let y2 = props.jump.to * props.lineHeight + props.lineHeight / 2;
+  return (
+    <React.Fragment key={props.jump.from}>
+      <polyline
+        fill="none"
+        strokeWidth={8}
+        stroke="var(--color-bg)"
+        points={`${x1},${y1} ${x2},${y1} ${x2},${y2} ${x1},${y2}`}
+      />
+      <polyline
+        fill="none"
+        strokeWidth={2}
+        stroke={
+          !props.current
+            ? "var(--color-comment)"
+            : props.jump.broken
+            ? "var(--color-dots)"
+            : "currentColor"
+        }
+        markerEnd={`url(${
+          !props.current
+            ? "#head-grey"
+            : props.jump.broken
+            ? "#head-red"
+            : "#head-green"
+        })`}
+        points={`${x1},${y1} ${x2},${y1} ${x2},${y2} ${x1},${y2}`}
+      />
+    </React.Fragment>
+  );
+};
+
 export const Arrows = (props: {
   instructions: SimilarInstructions[];
   current?: number;
   skip: { from: number; length: number };
 }) => {
-  const fontSize = parseInt(window.getComputedStyle(document.body)["fontSize"]);
+  const fontSize = useMemo(
+    () => parseInt(window.getComputedStyle(document.body)["fontSize"]),
+    []
+  );
   const lineHeight = fontSize * 1.3;
   const height = lineHeight * (props.instructions.length + props.skip.length);
 
   const packedJumps = useMemo(() => {
-    const jumps = props.instructions.flatMap((e, i) => {
-      if (e.instructions.length < 1) {
-        return [];
-      }
-      const jump = e.instructions[0].actualJump;
-      if (jump.label === "concrete") {
-        return [{ from: i, to: i + jump.distance, broken: false }];
-      }
-      if (jump.label === "between") {
-        return [{ from: i, to: i + jump.distance, broken: true }];
-      }
-      return [];
-    });
+    const jumps = instructions2jumps(props.instructions)
     const packedJumps = packJumps(jumps);
     packedJumps.reverse();
 
@@ -90,42 +164,6 @@ export const Arrows = (props: {
   const width =
     (packedJumps.reduce((a, b) => Math.max(a, b.level), 0) + 1) * fontSize;
 
-  const renderJump = (jump: LevelledJump) => {
-    let x1 = width - 1;
-    let x2 = x1 - fontSize * jump.level;
-    let y1 = jump.from * lineHeight + lineHeight / 2;
-    let y2 = jump.to * lineHeight + lineHeight / 2;
-    return (
-      <React.Fragment key={jump.from}>
-        <polyline
-          fill="none"
-          strokeWidth={8}
-          stroke="var(--color-bg)"
-          points={`${x1},${y1} ${x2},${y1} ${x2},${y2} ${x1},${y2}`}
-        />
-        <polyline
-          fill="none"
-          strokeWidth={2}
-          stroke={
-            props.current && props.current !== jump.from
-              ? "var(--color-comment)"
-              : jump.broken
-              ? "var(--color-dots)"
-              : "currentColor"
-          }
-          markerEnd={`url(${
-            props.current && props.current !== jump.from
-              ? "#head-grey"
-              : jump.broken
-              ? "#head-red"
-              : "#head-green"
-          })`}
-          points={`${x1},${y1} ${x2},${y1} ${x2},${y2} ${x1},${y2}`}
-        />
-      </React.Fragment>
-    );
-  };
-
   return (
     <svg
       className="arrows"
@@ -133,67 +171,32 @@ export const Arrows = (props: {
       style={{ width: `${width}px`, height: `${height}px` }}
     >
       <defs>
-        <marker
-          id="head-green"
-          orient="auto"
-          markerUnits="userSpaceOnUse"
-          markerWidth="9"
-          markerHeight="16"
-          refX="8"
-          refY="8"
-        >
-          <polyline
-            strokeWidth="2"
-            points="1,1 8,8 1,15"
-            fill="none"
-            stroke={"currentColor"}
-            strokeLinejoin="round"
-            strokeLinecap="round"
-          ></polyline>
-        </marker>
-        <marker
-          id="head-red"
-          orient="auto"
-          markerUnits="userSpaceOnUse"
-          markerWidth="9"
-          markerHeight="16"
-          refX="8"
-          refY="8"
-        >
-          <polyline
-            strokeWidth="2"
-            points="1,1 8,8 1,15"
-            fill="none"
-            stroke={"var(--color-dots)"}
-            strokeLinejoin="round"
-            strokeLinecap="round"
-          ></polyline>
-        </marker>
-        <marker
-          id="head-grey"
-          orient="auto"
-          markerUnits="userSpaceOnUse"
-          markerWidth="9"
-          markerHeight="16"
-          refX="8"
-          refY="8"
-        >
-          <polyline
-            strokeWidth="2"
-            points="1,1 8,8 1,15"
-            fill="none"
-            stroke={"var(--color-comment)"}
-            strokeLinejoin="round"
-            strokeLinecap="round"
-          ></polyline>
-        </marker>
+        <Marker id="head-green" color="currentColor" />
+        <Marker id="head-red" color="var(--color-dots)" />
+        <Marker id="head-grey" color="var(--color-comment)" />
       </defs>
       {packedJumps
         .filter((jump) => jump.from !== props.current)
-        .map(renderJump)}
+        .map((jump) => (
+          <JumpElement
+            width={width}
+            lineHeight={lineHeight}
+            fontSize={fontSize}
+            jump={jump}
+            current={!props.current}
+          />
+        ))}
       {packedJumps
         .filter((jump) => jump.from === props.current)
-        .map(renderJump)}
+        .map((jump) => (
+          <JumpElement
+            width={width}
+            lineHeight={lineHeight}
+            fontSize={fontSize}
+            jump={jump}
+            current={true}
+          />
+        ))}
     </svg>
   );
 };
