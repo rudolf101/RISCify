01496f09f7792d374154ce970a8c8487b6028f8d
Dark Hole <dark.hole1@yandex.ru>
2025-06-16 19:51:02 +0300

    Add styling

--- app/src/ui/About.tsx
+++ app/src/ui/About.tsx
@@ -1,13 +1,17 @@
+import "./About.css";
+
 export const About = () => (
-  <div>
-    <p>RISCify - online RISC-V disassembler.</p>
+  <div className="about">
     <p>
-      GitHub:{" "}
+      <span className="name">RISCify</span> - online RISC-V disassembler.
+    </p>
+    <p>
+      <span className="special">GitHub:</span>{" "}
       <a href="https://github.com/rudolf101/RISCify">
         https://github.com/rudolf101/RISCify
       </a>
     </p>
-    <p>Authors:</p>
+    <p className="special">Authors:</p>
     <ul>
       <li>Akim Byrkov</li>
       <li>Andrey Bogdanov</li> <li>Andrey Oleynikov</li>
