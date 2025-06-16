a9f34fcc3fe51ca0e0c5bbdd4d41c92b697d7f6c
Dark Hole <dark.hole1@yandex.ru>
2025-06-16 19:24:04 +0300

    Add basic about

---
+++ app/src/ui/About.tsx
@@ -0,0 +1,20 @@
+export const About = () => (
+  <div>
+    <p>RISCify - online RISC-V disassembler.</p>
+    <p>
+      GitHub:{" "}
+      <a href="https://github.com/rudolf101/RISCify">
+        https://github.com/rudolf101/RISCify
+      </a>
+    </p>
+    <p>Authors:</p>
+    <ul>
+      <li>Akim Byrkov</li>
+      <li>Andrey Bogdanov</li> <li>Andrey Oleynikov</li>
+      <li>Vladislav Kasimov</li>
+      <li>Elizaveta Konovkina</li>
+      <li>Roman Deev</li>
+      <li>Stepan Avdeev</li>
+    </ul>
+  </div>
+);
