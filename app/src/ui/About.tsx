42000c39acab01ee896224c9fffaef9e4f3ed49b
Dark Hole <dark.hole1@yandex.ru>
2025-06-17 12:13:15 +0300

    Add about page (#158)

---
+++ app/src/ui/About.tsx
@@ -0,0 +1,25 @@
+import "./About.css";
+
+export const About = () => (
+  <div className="about">
+    <p>
+      <span className="name">RISCify</span> - online RISC-V disassembler.
+    </p>
+    <p>
+      <span className="special">GitHub:</span>{" "}
+      <a href="https://github.com/rudolf101/RISCify">
+        https://github.com/rudolf101/RISCify
+      </a>
+    </p>
+    <p className="special">Authors:</p>
+    <ul>
+      <li>Akim Byrkov</li>
+      <li>Andrey Bogdanov</li>
+      <li>Andrey Oleynikov</li>
+      <li>Elizaveta Konovkina</li>
+      <li>Roman Deev</li>
+      <li>Stepan Avdeev</li>
+      <li>Vladislav Kasimov</li>
+    </ul>
+  </div>
+);
